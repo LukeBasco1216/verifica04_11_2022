67b9846b141d78bd828291a44a5483237b2777f1
LukeBasco1216 <basco.luke@iisgalvanimi.edu.it>
2022-11-08 07:31:02 +0000

    fila 3

--- correzione-fila3/app.py
+++ correzione-fila3/app.py
@@ -3,45 +3,33 @@
 #   si deve poter visualizzare l'elenco degli ordini effettuati. Utilizzare Bootstrap per l'interfaccia grafica.
 
 
-from flask import Flask, render_template, request, redirect, url_for, Response, redirect
+from flask import Flask, render_template, request, Response
 app = Flask(__name__)
 
 import io
+import pandas as pd 
+import matplotlib.pyplot as plt
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 import matplotlib
 matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-import pandas as pd
 import pymssql
 
-#collegamento al DB
-connection = pymssql.connect(server="213.140.22.237\SQLEXPRESS", user="basco.luke",password="xxx123##",database="basco.luke")
+conn = pymssql.connect(server='213.140.22.237\SQLEXPRESS', user='porta.matteo', password='xxx123##', database='porta.matteo')
 
-@app.route('/bestCustomers', methods=['GET'])
-def table():
-    # query= select *
-    # from sales.customers as C inner join sales.orders as O
-    # on C.customer_id = O.customer_id
-    # inner join sales.order_items as OI
-    # on O.order_id = OI.order_id
-    return render_template("table.html")
+@app.route("/bestCustomers", methods=["GET"])
+def bestCustomers():
+    query = 'SELECT TOP 10 customers.customer_id, customers.first_name, customers.last_name ,SUM(list_price * sales.order_items.quantity) as spesa_totale FROM sales.customers INNER JOIN sales.orders ON sales.customers.customer_id = sales.orders.customer_id INNER JOIN sales.order_items ON sales.order_items.order_id = sales.orders.order_id GROUP BY customers.customer_id, customers.first_name, customers.last_name ORDER BY SUM(list_price * sales.order_items.quantity) DESC '
+    #INNER JOIN sales.oder_items ON sales.orders.order_id = sales.order_items.order_id
+    tabella = pd.read_sql(query,conn)
+    print(tabella.columns.values)
+    return render_template("bestCustomers.html", nomiColonne = tabella.columns.values, dati = tabella.values)
 
-
-@app.route('/inputval', methods=['GET'])
-def serv():
-  nome = request.args["nome"]
-  cognome = request.args["cognome"]
-  query = f"select * from sales.customers where sales.customers.first_name = '{nome}' and sales.customers.last_name = '{cognome}'" 
-  df1 = pd.read_sql(query, connection)
-  # controllo se un df è vuoto, semplice semplice
-  if list(df1.values.tolist()) == []: # se dati è una lista vuota
-    return render_template("error.html")
-  else:
-    return render_template("risultati1.html", nomicolonne = df1.columns.values, dati = list(df1.values.tolist()))
-
-
-
+@app.route("/totaleOrdini/<valore>")
+def totaleOrdini(valore):
+    query = f'SELECT * FROM sales.customers INNER JOIN sales.orders ON sales.customers.customer_id = sales.orders.customer_id WHERE customers.customer_id = {valore}'
+    tabella = pd.read_sql(query,conn)
+    return render_template("totaleOrdini.html", nomiColonne = tabella.columns.values, dati = tabella.values)
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=3235, debug=True)
+  app.run(host='0.0.0.0', port=3245, debug=True)
